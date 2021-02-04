319bba88e033a5d3fbcf1d9afcd84b3f49888f55
dungltr <dung80@gmai.com>
2021-02-04 15:54:26 +0100

    deploy on git@github.com:dungltr/hanoi-phap.git

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+npm install --save axios vue-axios vuex bootstrap bootstrap-vue
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:dungltr/hanoi-phap.git master:gh-pages
+
+cd -
